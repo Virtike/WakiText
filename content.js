34331fc63f8ecafba124663c3032562f3cc22d2f
Virtike <ride.isaak@gmail.com>
2019-08-07 15:31:42 +1000

    Add files via upload

---
+++ content.js
@@ -0,0 +1,8 @@
+var fontfile = chrome.extension.getURL("font.ttf");
+
+$(function (){
+  $('head').append('<style>@font-face {font-family:"Waki-Crayon-Font"; src: url( "' + fontfile + '" )}');
+  var path = chrome.extension.getURL('styles.css');
+  var style={"font-family":"Waki-Crayon-Font","font-size":"25px","line-height": "24px","letter-spacing":"2px"};
+  $("a.replyusername:contains('WAKIdesigns')").css(style).parent().siblings(".comtext").css(style);
+});
